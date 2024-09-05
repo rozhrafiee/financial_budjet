07c1928b2a25cc732fae010e2c4b126d16b1b018
Your Name <you@example.com>
2024-09-05 02:16:07 -0700

    add def

--- saving_paln/views.py
+++ saving_paln/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 import json
+import datetime
 from .models import SavingPlan, TotalSaving
 from .serializers import SavingPlanSerializer, TotalSavingSerializer
 from rest_framework.generics import (
@@ -25,3 +26,9 @@ class SavingPlanDetail(RetrieveUpdateDestroyAPIView):
 class TotalSavingDetail(RetrieveUpdateDestroyAPIView):
     queryset = TotalSaving.objects.all()
     serializer_class = TotalSavingSerializer
+    
+def make_totel_saving(request):
+    money = SavingPlan.monthly_saving *(SavingPlan.start_month -datetime)
+    TotalSaving.totel_saving = money
+    TotalSaving.totel_saving.save()
+    
