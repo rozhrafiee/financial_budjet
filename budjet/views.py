89454d90f32874da6bd5a0f251e5c849a30f4853
Your Name <you@example.com>
2024-09-05 01:35:25 -0700

    add wiew

--- budjet/views.py
+++ budjet/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView , CreateAPIView,ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import TitleSerializer
+from .models import Title , Budejet
+from rest_framework.permissions import IsAuthenticated , IsAdminUser ,AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView , token_refresh
+
+
+class BudjetDetail(ListCreateAPIView):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        return Title.objects.filter(user=self.request.user)
 
 # Create your views here.
