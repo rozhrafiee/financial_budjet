c1f9e1a2dcf9c6db4969347911b703b21e05e103
rozhrafiee <rozhinarafiee014@gmail.com>
2024-09-05 12:55:38 +0430

    miow

--- financial_budjet/settings.py
+++ financial_budjet/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'budjet',
+    'saving_plan'
+    'transaction'
 ]
 
 MIDDLEWARE = [
